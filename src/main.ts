506d8baee892763d30083214bf860a579b73c6af
Abdulmelik619 <111171040+Abdulmelik619@users.noreply.github.com>
2023-10-19 00:22:38 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule, httpsOptions: {
+      key: fs.readFileSync('/home/ubuntu/live/furqan.abdulmelikambaw.tech/privkey.pem'),
+      cert: fs.readFileSync('/home/ubuntu/live/furqan.abdulmelikambaw.tech/cert_private_key.pem'),
+    }, {cors: true});
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
